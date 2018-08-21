25d9413db27406a25ab67850d6d512fa2bfc85b4
Samuli Kakonen <samuli.kakonen@metropolia.fi>
2018-08-21 08:46:28 +0000

    01 Tehtäväsarjan tehtävät tehty

--- w1 JavaScript Language/01 JavaScript Tools/js/guess.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/guess.js
@@ -1,23 +1,41 @@
 let targetNumber = Math.floor(Math.random() * 10) + 1;
+let guesses = 1;
 
 function init () {
-	console.log("You have to code this!");
+    console.log(targetNumber);
+	document.getElementById("submitBtn").onclick = function(){
+	   var number = document.getElementById("number").value;
+	   check(number);
+	};
 }
 
 function check (value) {
-  
+  console.log("Checking...");
+  if (value == targetNumber) {
+      showWin();
+  }else if(value != targetNumber && guesses > 4){
+      showLoss();
+  }else if (value != targetNumber){
+      showError();
+      guesses++;
+  }
 }
 
 function showWin () {
-  
+  console.log("You Win!");
+  document.body.innerHTML = "<p>You Win!</p>";
 }
 
 function showError () {
-  
+  console.log("Wrong Number!");
+  document.getElementById("message").innerHTML = "Wrong Number!";
 }
 
 function showLoss () {
-  
+  console.log("You Lose!");
+  document.body.innerHTML = "<p>You Lose!</p>";
 }
 
-init();
+window.onload = function(){
+    init();
+}
